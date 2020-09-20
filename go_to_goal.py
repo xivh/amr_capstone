dd3e7de45599f24ed7ca1c48f8213948509824ac
katiekleeman <51762906+katiekleeman@users.noreply.github.com>
2020-09-20 13:57:08 -0400

    updated 9/20

--- go_to_goal.py
+++ go_to_goal.py
@@ -1,6 +1,9 @@
+#! /usr/bin/env python
+
 import rospy
-from geometry_msgs.msg  import Twist
-from math import pow,atan2,sqrt
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from math import pow, atan2, sqrt
 import numpy as np
 from sensor_msgs.msg import Imu
 from tf.transformations import euler_from_quaternion
@@ -9,24 +12,26 @@ from tf.transformations import euler_from_quaternion
 class pure_pursuit():
     def __init__(self):
         # Creating our node,publisher and subscriber
-        # rospy.init_node('turtlebot_controller', anonymous=True)
-        self.velocity_publisher = rospy.Publisher('/husky_velocity_controller/cmd_vel', Twist, queue_size=10)
-        self.states_subscriber = rospy.Subscriber('/gazebo/model_states', ModelStates, self.statesCallback)
+        rospy.init_node('pure_pursuit_cap', anonymous=True)
+        self.velocity_publisher = rospy.Publisher('/husky_velocity_controller/cmd_vel', Twist, queue_size=1)
+        self.states_subscriber = rospy.Subscriber('/odometry/filtered', Odometry, self.statesCallback)
         self.rate = rospy.Rate(10)
 
     # Callback function implementing the pose value received
     def statesCallback(self, data):
-        self.x = data.pose.position.x
-        self.y = data.pose.position.y
-        self.v = data.twist.linear.x
+        self.x = data.pose.pose.position.x
+        self.y = data.pose.pose.position.y
+        self.v = data.twist.twist.linear.x
         quaternion = (
-            data.pose.orientation.x,
-            data.pose.orientation.y,
-            data.pose.orientation.z,
-            data.pose.orientation.w
+            data.pose.pose.orientation.x,
+            data.pose.pose.orientation.y,
+            data.pose.pose.orientation.z,
+            data.pose.pose.orientation.w
         )
-        euler = tf.transformations.euler_from_quaternion(quaternion)
+        euler = euler_from_quaternion(quaternion)
         self.yaw = euler[2]
+        # print("curr_x: %f", self.x)
+        # print("curr_y: %f", self.y)
         # rospy.loginfo("Angular Velocity = %f", data.angular_velocity.z)
 
     def get_distance(self, goal_x, goal_y):
@@ -34,11 +39,13 @@ class pure_pursuit():
         return distance
 
     def move2goal(self, goal_pose_x, goal_pose_y):
-        distance_tolerance = 0.1
+        distance_tolerance = 0.5
         vel_msg = Twist()
-
+        print("goal_x: %f", goal_pose_x)
+        print("goal_y: %f", goal_pose_y)
+        print("curr_x: %f", self.x)
+        print("curr_y: %f", self.y)
         while sqrt(pow((goal_pose_x - self.x), 2) + pow((goal_pose_y - self.y), 2)) >= distance_tolerance:
-
             # Porportional Controller
             # linear velocity in the x-axis:
             vel_msg.linear.x = 1.5 * sqrt(pow((goal_pose_x - self.x), 2) + pow((goal_pose_y - self.y), 2))
@@ -49,11 +56,18 @@ class pure_pursuit():
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
             vel_msg.angular.z = 4 * (atan2(goal_pose_y - self.y, goal_pose_x - self.x) - self.yaw)
-
+            print("before publish")
             # Publishing our vel_msg
             self.velocity_publisher.publish(vel_msg)
+            print("angular: %f", vel_msg.angular.z)
+            print("linear: %f", vel_msg.linear.x)
+            print("before sleep")
             self.rate.sleep()
+            print("before spin")
+            rospy.spin()
+            print("after spin")
         # Stopping our robot after the movement is over
+        print("out of loop")
         vel_msg.linear.x = 0
         vel_msg.angular.z = 0
         self.velocity_publisher.publish(vel_msg)
@@ -66,13 +80,13 @@ if __name__ == '__main__':
         # Testing our function
         x = pure_pursuit()
         rospy.spin()
-        goals_x = []
-        goals_y = []
+        goals_x = [10]
+        goals_y = [0]
         i = 0
-        while i < len(target_x):
+        while i < len(goals_x):
             gx = goals_x[i]
             gy = goals_y[i]
-            x.move2goal(gx, gy)
+            x.move2goal(10, 0)
             i += 1
 
     except rospy.ROSInterruptException: pass
