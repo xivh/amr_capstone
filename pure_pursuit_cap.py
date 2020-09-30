eb1e8a510563c36ba63ced5aa87a53daaf733c8b
katiekleeman <51762906+katiekleeman@users.noreply.github.com>
2020-09-30 14:06:20 -0400

    updated for testing

--- pure_pursuit_cap.py
+++ pure_pursuit_cap.py
@@ -4,39 +4,29 @@ import rospy
 from geometry_msgs.msg import Twist
 import numpy as np
 from nav_msgs.msg import Odometry
-from gazebo_msgs.msg import ModelStates
 from math import pow, atan2, sqrt, ceil
 from tf.transformations import euler_from_quaternion
 
-x = 0.0
-y = 0.0
-v = 0.0
-yaw = 0.0
 
 def statesCallback(data):
     global x, y, v, yaw
-    # find index of slash
-    name = data.name
-    index = name.index("jackal")
-    #index = name.index("/")
-    x = data.pose[index].position.x
-    y = data.pose[index].position.y
-    v = data.twist[index].linear.x
+    x = data.pose.pose.position.x
+    y = data.pose.pose.position.y
+    v = data.twist.twist.linear.x
     quaternion = (
-        data.pose[index].orientation.x,
-        data.pose[index].orientation.y,
-        data.pose[index].orientation.z,
-        data.pose[index].orientation.w
+        data.pose.pose.orientation.x,
+        data.pose.pose.orientation.y,
+        data.pose.pose.orientation.z,
+        data.pose.pose.orientation.w
     )
     euler = euler_from_quaternion(quaternion)
     yaw = euler[2]
 
+
 def robotAtGoal(robx, roby, goalx, goaly):
-    distance_tolerance = 1
-    val = sqrt(pow((goalx - robx), 2) + pow((goaly - roby), 2)) 
-    
-    print("distance:", val, val <= distance_tolerance)
-    return val <= distance_tolerance
+    distance_tolerance = 0.5
+    return sqrt(pow((goalx - robx), 2) + pow((goaly - roby), 2)) <= distance_tolerance
+
 
 def getLookAheadPoint(waypoints, robx, roby, lookAheadDistance, lastIndex, lastFractionalIndex, lastLookAhead):
     for j in range(lastIndex, len(waypoints) - 1):
@@ -50,35 +40,29 @@ def getLookAheadPoint(waypoints, robx, roby, lookAheadDistance, lastIndex, lastF
         b = np.dot(np.multiply(2, f), d)
         c = np.dot(f, f) - r*r
         discriminant = b*b - 4*a*c
-        
-        # this happens on the first waypoint, since the lookahead distance is smaller
         if discriminant < 0:
-            return lastLookAhead, lastIndex, lastFractionalIndex
-
+            break
         discriminant = sqrt(discriminant)
         t1 = (-b - discriminant)/(2*a)
         t2 = (-b + discriminant)/(2*a)
-        print('t guys', t1, t2)
         if 0 <= t1 <= 1 and j+t1 > lastFractionalIndex:
-            return (E[0] + t1*d[0], E[1] + t1*d[1]), j, j+t1
+            return (E[0] + t1*d, E[1] + t1*d), j, j+t1
         if 0 <= t2 <= 1 and j+t2 > lastFractionalIndex:
-            return (E[0] + t2*d[0], E[1] + t2*d[1]), j, j+t2
+            return (E[0] + t2*d, E[1] + t2*d), j, j+t2
+        return lastLookAhead
 
-        # this happens on the last waypoint. I'm not sure if j should be updated on the two
-        # return statements above? or if this solution is best - we should figure out why
-        # lookahead points aren't working for the first and last waypoint
-        return waypoints[lastIndex+1], j+1, lastFractionalIndex
-    return waypoints[-1], lastIndex, lastFractionalIndex
-        
+
+
+
 def injectPoints(waypoints):
-    spacing = 5
+    spacing = 0.2
     new_points = []
     for j in range(0, len(waypoints)-1):
         start_point = waypoints[j]
         end_point = waypoints[j+1]
         vector = (end_point[0] - start_point[0], end_point[1] - start_point[1])
         d = sqrt(pow(vector[0], 2)+pow(vector[1], 2))
-        num_points_that_fit = int(ceil(d/spacing))
+        num_points_that_fit = ceil(d/spacing)
         vector = (vector[0]/d * spacing, vector[1]/d * spacing)
         for i in range(0, num_points_that_fit):
             new_list = (start_point[0] + vector[0]*i, start_point[1] + vector[1]*i)
@@ -86,53 +70,49 @@ def injectPoints(waypoints):
         new_points.append(end_point)
     return new_points
 
-def smoothPath(path): # path is [(x1, y1), ..., (xend, yend)]
+
+def smoothPath(path):
     b = 0.75
     a = 1-b
     tolerance = 0.001
-    newPath = [list(point) for point in path] # tuples are immutable
+    newPath = path
     change = tolerance
     while change >= tolerance:
         change = 0
         for i in range(1, len(path)-1):
             for j in range(0, len(path[i])):
-                aux = newPath[i][j]
-                newPath[i][j] += a*(path[i][j] - newPath[i][j]) + b*(newPath[i-1][j] + newPath[i+1][j]
-                                                                     - (2.0*newPath[i][j]))
-                change += abs(aux-newPath[i][j])
-    newPath = list(tuple(point) for point in newPath)
+                aux = newPath[i, j]
+                newPath[i, j] += a*(path[i, j] - newPath[i, j]) + b*(newPath[i-1, j] + newPath[i+1, j]
+                                                                     - (2.0*newPath[i, j]))
+                change += abs(aux-newPath[i, j])
     return newPath
 
+
 def main():
-    #global x, y, v, yaw
     rospy.init_node('pure_pursuit_cap', anonymous=True)
-    velocity_publisher = rospy.Publisher('/jackal_velocity_controller/cmd_vel', Twist, queue_size=10)
-    #velocity_publisher = rospy.Publisher('/husky_velocity_controller/cmd_vel', Twist, queue_size=10)
-    rospy.Subscriber('/gazebo/model_states', ModelStates, statesCallback)
+    velocity_publisher = rospy.Publisher('/husky_velocity_controller/cmd_vel', Twist, queue_size=10)
+    rospy.Subscriber('/odometry/filtered', Odometry, statesCallback)
     rate = rospy.Rate(10)
     vel_msg = Twist()
-    waypoints = [(10, 0), (0, 10), (10, 10), (0, 0)]
+    waypoints = [(0, 1), (8, 0)]
     path = injectPoints(waypoints)
-    # goals_x = [10]
-    # goals_y = [0]
-    lookAheadDistance = 2
+    goals_x = [10]
+    goals_y = [0]
+    lookAheadDistance = 20
     lastIndex = 0
     lastLookAheadIndex = 0
-    lastFractionalIndex = 0
-    lookAheadPoint = waypoints[0]
     i = 0
-    while not rospy.is_shutdown():
-        # print('goals:', waypoints[-1][0], waypoints[-1][1])
-        if robotAtGoal(x, y, waypoints[-1][0], waypoints[-1][1]) and lastIndex == len(waypoints) - 1: 
-           vel_msg.linear.x = 0
-           vel_msg.angular.z = 0
-           velocity_publisher.publish(vel_msg)
-           break 
 
-        lookAheadPoint, lastIndex, lastFractionalIndex = getLookAheadPoint(waypoints, x, y, lookAheadDistance, lastIndex, lastFractionalIndex, lookAheadPoint)
+    while not rospy.is_shutdown():
+        #if robotAtGoal(x, y, goal_pose_x, goal_pose_y):  # switch to next goal or not
+        #    goal_pose_x = goals_x[i]
+        #    goal_pose_y = goals_y[i]
+        #    i += 1
+
+        lookAheadPoint, lastIndex, lastFractionalIndex = getLookAheadPoint(waypoints, x, y, lookAheadDistance,
+                                                              lastIndex, lastFractionalIndex, lookAheadPoint)
         goal_pose_x = lookAheadPoint[0]
         goal_pose_y = lookAheadPoint[1]
-        print('lookahead:', lookAheadPoint, 'index:', lastIndex)
         # main_logic to compute linear_x, angular_z
         # Proportional Controller
         # linear velocity in the x-axis:
@@ -144,17 +124,17 @@ def main():
         # angular velocity in the z-axis:
         vel_msg.angular.x = 0
         vel_msg.angular.y = 0
-        vel_msg.angular.z = 2 * (atan2(goal_pose_y - y, goal_pose_x - x) - yaw)
-        #print("before publish")
+        vel_msg.angular.z = 4 * (atan2(goal_pose_y - y, goal_pose_x - x) - yaw)
+        print("before publish")
 
         # Publishing our vel_msg
         velocity_publisher.publish(vel_msg)
-        print("x: %f", x)
-        print("y: %f", y)
-        #print("before sleep")
+        print("angular: %f", vel_msg.angular.z)
+        print("linear: %f", vel_msg.linear.x)
+        print("before sleep")
         rate.sleep()
-        #print("before spin")
-        #rospy.spin() # vehicle will not
+        print("before spin")
+
 
 if __name__ == "__main__":
     main()
