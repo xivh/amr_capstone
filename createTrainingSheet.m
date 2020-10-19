0bd889aeb15f6ce915ceee98d4f5a36b89058fe7
Grace Glaubit <gsg6bu@virginia.edu>
2020-10-18 21:33:56 -0400

    Update createTrainingSheet.m
    fixing angle issue

--- createTrainingSheet.m
+++ createTrainingSheet.m
@@ -40,33 +40,56 @@ for i = 1:length(bags)
         vel = 2.0;
     end
 
-    % calculate angle
     run_list = [1 2 3 4 5 6 7 8 9 10 121 122 123 124 125 126 127 128 129 130 241 242 243 244 245 246 247 248 249 250];
     angle = 0;
     if ismember(run, run_list)
         angle = 0;
+        goal_x = 20;
+        goal_y = 0;
     elseif ismember(run, run_list+10)
         angle = 15;
+        goal_x = 19.659258260000000;
+        goal_y = 2.588190451000000;
     elseif ismember(run, run_list+20)
         angle = 30;
+        goal_x = 18.660254040000000;
+        goal_y = 5;
     elseif ismember(run, run_list+30)
         angle = 45;
+        goal_x = 17.071067810000000;
+        goal_y = 7.071067812000000;
     elseif ismember(run, run_list+40)
         angle = 60;
+        goal_x = 15;
+        goal_y = 8.660254038000000;
     elseif ismember(run, run_list+50)
         angle = 75;
+        goal_x = 12.588190450000000;
+        goal_y = 9.659258263000000;
     elseif ismember(run, run_list+60)
         angle = 90;
+        goal_x = 10;
+        goal_y = 10;
     elseif ismember(run, run_list+70)
         angle = 105;
+        goal_x = 7.411809549000000;
+        goal_y = 9.659258263000000;
     elseif ismember(run, run_list+80)
         angle = 120;
+        goal_x = 5;
+        goal_y = 8.660254038000000;
     elseif ismember(run, run_list+90)
         angle = 135;
+        goal_x = 2.928932188000000;
+        goal_y = 7.071067812000000;
     elseif ismember(run, run_list+100)
         angle = 150;
+        goal_x = 1.339745962000000;
+        goal_y = 5;
     elseif ismember(run, run_list+110)
         angle = 165;
+        goal_x = 0.340741737100000;
+        goal_y = 2.588190451000000;
     end
 
     % calculate mu
@@ -97,16 +120,16 @@ for i = 1:length(bags)
     y = cellfun(@(m) (m.Pose(index).Position.Y),msgStructs1)';
     x_final = x(1, length(x));
     y_final = y(1, length(y));
-    filename = 'C:\Users\Student\Documents\UVA\Year 4\Capstone\bag_reader_capstone\Training Bags 10.18\goals.csv';
-    goals = importdata(filename);
-    s = size(goals);
-    rows = size(1);
-    for j = 1:rows
-        if angle == goals(j,1)
-            goal_x = goals(j,2);
-            goal_y = goals(j,3);
-        end
-    end
+    % filename = 'C:\Users\Student\Documents\UVA\Year 4\Capstone\bag_reader_capstone\Training Bags 10.18\goals.csv';
+    % goals = importdata(filename);
+    % s = size(goals);
+    % rows = size(1);
+    % for j = 1:rows
+    %    if angle == goals(j,1)
+    %        goal_x = goals(j,2);
+    %        goal_y = goals(j,3);
+    %    end
+    % end
     dist = sqrt((goal_x - x_final).^2 + (goal_y - y_final).^2);
     
     % calculate success
